3eda900124cd54f9e2dc166a63212a2267ae0e44
Manish Arora <manish.arora1392@gmail.com>
2018-09-04 18:52:03 -0700

    Testing using spies

---
+++ spies/app.js
@@ -0,0 +1,9 @@
+const db = require('./db');
+
+
+module.exports.handleSignup = (email,password) =>{
+    //Check if email already exists
+    //Save user to db
+    db.saveUser({email,password});
+    //Send the welcome mail
+};
